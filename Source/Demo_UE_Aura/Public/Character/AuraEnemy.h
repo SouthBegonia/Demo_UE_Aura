5be36789a7427ab560690b64ef51e16fc272474a
SouthBegonia <southbegonia@foxmail.com>
2025-10-27 18:01:47 +0800

    新加 Aura/Goblin的基类及BP

---
+++ Source/Demo_UE_Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMO_UE_AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
