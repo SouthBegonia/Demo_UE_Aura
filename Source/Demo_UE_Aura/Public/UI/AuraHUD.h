d7ad040bbf3b14c36c628d1ee1348f64b885d537
SouthBegonia <southbegonia@foxmail.com>
2025-10-30 10:27:13 +0800

    新加 HUD

---
+++ Source/Demo_UE_Aura/Public/UI/AuraHUD.h
@@ -0,0 +1,29 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+class UAuraUserWidget;
+/**
+ * 
+ */
+UCLASS()
+class DEMO_UE_AURA_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+
+public:
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+};
