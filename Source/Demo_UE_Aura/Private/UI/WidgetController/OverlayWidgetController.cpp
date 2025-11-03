d609d448f6f24ba87a909470bf656dfdee30d90e
SouthBegonia <southbegonia@foxmail.com>
2025-11-03 16:58:53 +0800

    新加 Message系列Tag用于处理GE通知表现层 新加 MessageWidgetData用于建立Message系列Tag对应的表现方式映射 新加 FMessageWidgetRowSignature委托以通知表现层GE触发

--- Source/Demo_UE_Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Demo_UE_Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -29,13 +29,19 @@ void UOverlayWidgetController::BindCallbackToDependencies()
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetManaAttribute()).AddUObject(this, &UOverlayWidgetController::ManaChanged);
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
 
-	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda([](const FGameplayTagContainer& AssetTags)
+	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda([this](const FGameplayTagContainer& AssetTags)
 	{
-		/*for (const FGameplayTag Tag : AssetTags)
+		for (const FGameplayTag Tag : AssetTags)
 		{
-			const FString Msg = FString::Printf(TEXT("GE Tag = %s"), *Tag.ToString());
-			GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
-		}*/
+			//const FString Msg = FString::Printf(TEXT("GE Tag = %s"), *Tag.ToString());
+			//GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+
+			if (Tag.MatchesTag(FGameplayTag::RequestGameplayTag(FName("Message"))))
+			{
+				const FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
+				MessageWidgetRowDelegate.Broadcast(*Row);
+			}
+		}
 	});
 }
 
--- Source/Demo_UE_Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Demo_UE_Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -3,14 +3,37 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
+class UAuraUserWidget;
+struct FGameplayTag;
 struct FOnAttributeChangeData;
+
+USTRUCT(BlueprintType)
+struct FUIWidgetRow : public FTableRowBase
+{
+	GENERATED_BODY();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayTag MessageTag = FGameplayTag();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FText Message = FText();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<UAuraUserWidget> MessageWidget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftObjectPtr<UTexture2D> Image = nullptr;
+};
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
 
 /**
  * 
@@ -39,9 +62,25 @@ public:
 	UPROPERTY(BlueprintAssignable, Category="Aura|GAS|Attributes")
 	FOnMaxManaChangedSignature OnMaxManaChanged;
 
+
+	UPROPERTY(BlueprintAssignable, Category="Aura|GAS|Message")
+	FMessageWidgetRowSignature MessageWidgetRowDelegate;
+
 protected:
 	void HealthChanged(const FOnAttributeChangeData& AttributeChangeData) const;
 	void MaxHealthChanged(const FOnAttributeChangeData& AttributeChangeData) const;
 	void ManaChanged(const FOnAttributeChangeData& AttributeChangeData) const;
 	void MaxManaChanged(const FOnAttributeChangeData& AttributeChangeData) const;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Aura|Widget Data")
+	TObjectPtr<UDataTable> MessageWidgetDataTable;
+
+	template<typename T>
+	T* GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag);
 };
+
+template <typename T>
+T* UOverlayWidgetController::GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag)
+{
+	return DataTable->FindRow<T>(Tag.GetTagName(), TEXT(""));
+}
