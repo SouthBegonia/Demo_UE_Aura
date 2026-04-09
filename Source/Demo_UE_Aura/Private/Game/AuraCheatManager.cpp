f60ac4614c1bfd654c18d20bf3c07e74777bc49e
SouthBegonia <southbegonia@foxmail.com>
2026-04-09 15:06:55 +0800

    新加 Cheat指令-添加EXP

--- Source/Demo_UE_Aura/Private/Game/AuraCheatManager.cpp
+++ Source/Demo_UE_Aura/Private/Game/AuraCheatManager.cpp
@@ -3,8 +3,13 @@
 
 #include "Game/AuraCheatManager.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "GameplayCueNotifyTypes.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Character/AuraCharacter.h"
 #include "Character/AuraEnemy.h"
 #include "Game/AuraEnemyManagerSubsystem.h"
+#include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
 
 void UAuraCheatManager::AuraEnemyFreeze() const
@@ -34,3 +39,62 @@ void UAuraCheatManager::AuraEnemyFreezeRecover()
 		UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("AuraEnemyFreezeRecover Done.")));
 	}
 }
+
+
+/**
+ * 添加指定EXP到Aura
+ *	- 示例：AuraPlayer.AddExp 1000
+ */
+static FAutoConsoleCommandWithWorldAndArgs AuraAddExpToPlayer(
+	TEXT("AuraPlayer.AddExp"),
+	TEXT("Add the specified EXP to Aura"),
+	FConsoleCommandWithWorldAndArgsDelegate::CreateLambda([](const TArray< FString >& Args, UWorld* World)
+	{
+		if (World)
+		{
+			if (Args.Num() == 0)
+				return;
+
+			const int32 Exp = FCString::Atoi(*Args[0]);
+			if (Exp > 0)
+			{
+				auto ApplyAddExpGE = [](UAbilitySystemComponent* TargetASC, const int32 InEXP)
+				{
+					if (TargetASC == nullptr)
+						return;
+
+					// ===== Create GE =====
+					FGameplayEffectContextHandle EffectContext = TargetASC->MakeEffectContext();
+					EffectContext.AddSourceObject(TargetASC->GetOwner());
+
+					UGameplayEffect* Effect = NewObject<UGameplayEffect>(GetTransientPackage(), FName(TEXT("AddEXP")));
+					Effect->DurationPolicy = EGameplayEffectDurationType::Instant;
+					int32 Index = Effect->Modifiers.Num();
+					Effect->Modifiers.Add(FGameplayModifierInfo());
+					FGameplayModifierInfo& ModifierInfo = Effect->Modifiers[Index];
+					ModifierInfo.ModifierMagnitude = FScalableFloat(InEXP);
+					ModifierInfo.ModifierOp = EGameplayModOp::Additive;
+					ModifierInfo.Attribute = UAuraAttributeSet::GetIncomingEXPAttribute();
+
+					// ===== Apply GE =====
+					if (FGameplayEffectSpec* MutableSpec = new FGameplayEffectSpec(Effect, EffectContext, 1.f))
+					{
+						TargetASC->ApplyGameplayEffectSpecToSelf(*MutableSpec);
+						UKismetSystemLibrary::PrintString(TargetASC, FString::Printf(TEXT("AuraPlayer.AddExp success. EXP = %d"), InEXP));
+					}
+				};
+
+				TArray<AActor*> AllActors;
+				UGameplayStatics::GetAllActorsOfClass(World, AAuraCharacter::StaticClass(), AllActors);
+				for (AActor* const& PlayerActor : AllActors)
+				{
+					if (PlayerActor->ActorHasTag(FName("Player")))
+					{
+						UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(PlayerActor);
+						ApplyAddExpGE(ASC, Exp);
+					}
+				}
+			}
+		}
+	})
+);
